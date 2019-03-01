e1d8721ac3a476dc72c27ce1db9526eca61e66ff
CesarRivasP <cesarjose58@gmail.com>
2019-02-28 21:09:07 -0400

    Added calculator

---
+++ 03-bases-node/app.js
@@ -0,0 +1,8 @@
+// tabla de multiplicar
+let base = 3;
+
+for(let i = 1; i <= 10; i++){
+  console.log(`${base} * ${i} = ${base * i}`);
+}
+
+//Node nos permite Grabar esta tabla de multiplicar y crear un archivo de texto con ese valor
