2ec80487f3c5d568fcc2a3f08d717ffe05bd3a59
CesarRivasP <cesarjose58@gmail.com>
2019-03-04 18:28:57 -0400

    Added createfile require module and others changes

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,27 +1,30 @@
-// Hay tres tipos de require
-const fs = require('fs'); //de un proyecto propio en node, es decir, solo definimos dicha libreria porque
-//la misma es nativa de node
-
-// const express = require('express'); No es una libreria que es propia de node, es decir, es un paquete
-// que se instala para su posterior utilizacion. No nativo de node, son expansiones, etc
-// const fs = require('fs'); archivos que creamos en el proyecto, se identifican pro ('./PATH')
-
-
 // tabla de multiplicar
-let base = 6;
+// console.log(module);
 
-let data= '';
+// const multiply = require('./multiply/multiply'); forma normal
+const { createFile } = require('./multiply/multiply');
 
-for(let i = 1; i <= 10; i++){
-  console.log(`${base} * ${i} = ${base * i}`);
-  data += `${base} * ${i} = ${base * i} \n`;
-}
+// console.log(multiply);// { createFile: [Function: createFile] }
 
-// fs.writeFile(nombreArchivo o PATH, data[, options como utf-8 ...], callback)
-fs.writeFile(`./tables/table-${base}.txt`, data, (err) => {
-  if (err) throw err; //si existe un error, mostramos el error
+let base = '5';
 
-  console.log('The file has been saved!');  //Si no, se indica que se creo el archivo
-})
+// let data= '';
+//
+// for(let i = 1; i <= 10; i++){
+//   console.log(`${base} * ${i} = ${base * i}`);
+//   data += `${base} * ${i} = ${base * i} \n`;
+// }
+//
+// // fs.writeFile(nombreArchivo o PATH, data[, options como utf-8 ...], callback)
+// fs.writeFile(`./tables/table-${base}.txt`, data, (err) => {
+//   if (err) throw err; //si existe un error, mostramos el error
+//
+//   console.log('The file has been saved!');  //Si no, se indica que se creo el archivo
+// })
+
+createFile(base)
+  .then((file) => console.log(`Archivo creado ${file}`))
+  .catch((err) => console.log(err));
 
 //Node nos permite Grabar esta tabla de multiplicar y crear un archivo de texto con ese valor
+//Los arrchivos base de las app de node deben de ser lo suficientemente simples, sin demasiada logica
