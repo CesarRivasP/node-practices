4eeca15268234481d8039931ed631bc305e5741d
CesarRivasP <cesarjose58@gmail.com>
2019-03-14 11:34:01 -0400

    Added logs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,8 @@
 // tabla de multiplicar
 // console.log(module);
 
+const argv = require('yargs').argv;  //after
+
 // const multiply = require('./multiply/multiply'); forma normal
 const { createFile } = require('./multiply/multiply');
 
@@ -25,19 +27,23 @@ const { createFile } = require('./multiply/multiply');
 //after
 // console.log(process.argv); // objeto global
 //argv = argumentos
-let argv = process.argv;
+let argv1 = process.argv;
+
+console.log(argv);  //argv de yargs -> { _: [ 'listar' ], base: 5, '$0': 'app' }. Retorna los comandos aislados
+
+console.log(argv1); //argv de los procesos
 //Al agregar el argumento en el argv este quedaria en el 3 puesto,
 //segundo del array, puesto que antes esta el path de node y el PATH
 //del archivo a ejecutar
-let param = argv[2];
+// let param = argv[2];
 // let base = param.split('=') -> se quiere separar por el signo de '=' -> ['--base', '5']
-let base = param.split('=')[1];
+// let base = param.split('=')[1];
 
 // console.log(base);
 
-createFile(base)
-  .then((file) => console.log(`Archivo creado ${file}`))
-  .catch((err) => console.log(err));
+// createFile(base)
+//   .then((file) => console.log(`Archivo creado ${file}`))
+//   .catch((err) => console.log(err));
 
 //Node nos permite Grabar esta tabla de multiplicar y crear un archivo de texto con ese valor
 //Los arrchivos base de las app de node deben de ser lo suficientemente simples, sin demasiada logica
