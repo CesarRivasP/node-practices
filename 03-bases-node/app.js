91b61fb9c9ef48456c28d3a50f04620e840f89aa
CesarRivasP <cesarjose58@gmail.com>
2019-03-14 11:37:26 -0400

    Delete comments

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -7,22 +7,6 @@ const argv = require('yargs').argv;  //after
 const { createFile } = require('./multiply/multiply');
 
 // console.log(multiply);// { createFile: [Function: createFile] }
-//before
-// let base = '5';
-
-// let data= '';
-//
-// for(let i = 1; i <= 10; i++){
-//   console.log(`${base} * ${i} = ${base * i}`);
-//   data += `${base} * ${i} = ${base * i} \n`;
-// }
-//
-// // fs.writeFile(nombreArchivo o PATH, data[, options como utf-8 ...], callback)
-// fs.writeFile(`./tables/table-${base}.txt`, data, (err) => {
-//   if (err) throw err; //si existe un error, mostramos el error
-//
-//   console.log('The file has been saved!');  //Si no, se indica que se creo el archivo
-// })
 
 //after
 // console.log(process.argv); // objeto global
@@ -44,6 +28,10 @@ console.log(argv1); //argv de los procesos
 // createFile(base)
 //   .then((file) => console.log(`Archivo creado ${file}`))
 //   .catch((err) => console.log(err));
+
+
+
+
 
 //Node nos permite Grabar esta tabla de multiplicar y crear un archivo de texto con ese valor
 //Los arrchivos base de las app de node deben de ser lo suficientemente simples, sin demasiada logica
