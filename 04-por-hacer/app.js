6d9ac02f0679d2522ceeb8560afd816f0dab1322
CesarRivasP <cesarjose58@gmail.com>
2019-03-19 12:53:37 -0400

    Added changes in yargs require and commands logs

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,19 +1,17 @@
-const argv = require('yargs').argv;
+const argv = require('./config/yargs').argv;
 // const colors = require('colors');
-
-console.log(argv);
 
 let command = argv._[0];
 
 switch (command) {
   case 'crear':
-    console.log('crear por hacer');
+    console.log('Crear un elemento por hacer');
     break;
   case 'listar':
-    console.log('listar todas las tareas por hacer');
+    console.log('Lista un elemento por hacer');
     break;
   case 'actualizar':
-    console.log('actualizar una tarea por hacer');
+    console.log('Actualiza el estado completado de una tarea');
     break;
   default:
     console.log('Comando no reconocido');
