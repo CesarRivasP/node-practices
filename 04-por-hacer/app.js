0df8154ee2425e7069ca3cef78c0e6d2e422645e
CesarRivasP <cesarjose58@gmail.com>
2019-03-19 11:43:50 -0400

    Added commands for using in comamnd line

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,4 +1,21 @@
-const argv = require('yargs');
-const colors = require('colors');
+const argv = require('yargs').argv;
+// const colors = require('colors');
 
 console.log(argv);
+
+let command = argv._[0];
+
+switch (command) {
+  case 'crear':
+    console.log('crear por hacer');
+    break;
+  case 'listar':
+    console.log('listar todas las tareas por hacer');
+    break;
+  case 'actualizar':
+    console.log('actualizar una tarea por hacer');
+    break;
+  default:
+    console.log('Comando no reconocido');
+    break;
+}
