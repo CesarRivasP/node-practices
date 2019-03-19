7bfb305ddcaa2baf624d52c16f89a63cb47d1af1
CesarRivasP <cesarjose58@gmail.com>
2019-03-19 13:24:33 -0400

    Added implementation to create function

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,11 +1,13 @@
 const argv = require('./config/yargs').argv;
 // const colors = require('colors');
+const todo = require('./to-do/to-do')
 
 let command = argv._[0];
 
 switch (command) {
   case 'crear':
-    console.log('Crear un elemento por hacer');
+    let task = todo.create(argv.descripcion)
+    console.log(task);
     break;
   case 'listar':
     console.log('Lista un elemento por hacer');
