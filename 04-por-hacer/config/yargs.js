8b15d3a8de27b10827b2a9ba5adb081e0c806f35
CesarRivasP <cesarjose58@gmail.com>
2019-03-19 12:53:53 -0400

    Added options and yargs commands

--- 04-por-hacer/config/yargs.js
+++ 04-por-hacer/config/yargs.js
@@ -1,1 +1,23 @@
-const argv = require('yargs').argv;
+const options = {
+  descripccion: {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripccion de la tarea por hacer'
+  },
+  completado: {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+  }
+}
+
+const argv = require('yargs')
+  .command('crear', 'Crear un elemento por hacer', options)
+  .command('actualizar', 'Actualiza el estado completado de una tarea', options)
+  .help()
+  .argv;
+
+
+module.exports = {
+  argv
+}
