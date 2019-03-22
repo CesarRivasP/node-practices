96829524f06da2700d6ede780865ab8988971a47
CesarRivasP <cesarjose58@gmail.com>
2019-03-22 11:24:27 -0400

    Added implementation of getInfo function

--- 05-world-weather/app.js
+++ 05-world-weather/app.js
@@ -1,11 +1,10 @@
 const argv = require('./config/yargs').argv;
-const places = require('./places/places');
-const weather = require('./weather/weather');
+// const places = require('./places/places');
+// const weather = require('./weather/weather');
+const info = require('./information/info');
+
 // console.log(argv.address); //after
 
-// places.getPlacesLngLat(argv.address)
-//   .then((response) => console.log(response));
-
-weather.getWeather(35.02, 139.01)
+info.getInfo(argv.address)
   .then((response) => console.log(response))
   .catch((err) => console.log(err))
