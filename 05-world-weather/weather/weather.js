704ecd3c07edea02951b8106484c4c77ef1528e0
CesarRivasP <cesarjose58@gmail.com>
2019-03-22 11:25:44 -0400

    Implemented apiWeather constant

--- 05-world-weather/weather/weather.js
+++ 05-world-weather/weather/weather.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-const key = require('../constants/constants');
+const apiWeatherKey = require('../constants/constants');
 
 const getWeather = async (lat, lng) => {
   //Solo es una peticion get, no hay que configurar headers porque el api key se indica en la URL
-  const response = await axios.get(`http://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key.API_KEY}`)
+  const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiWeatherKey.WEATHER_API_KEY}`)
 
   return response.data.main.temp;
 }
