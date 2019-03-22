47fc9da613fec6cb5d78e36e752409843d637bb6
CesarRivasP <cesarjose58@gmail.com>
2019-03-22 10:20:46 -0400

    Implemented getWeather function

--- 05-world-weather/app.js
+++ 05-world-weather/app.js
@@ -1,6 +1,11 @@
 const argv = require('./config/yargs').argv;
-const places = require('./places/places')
+const places = require('./places/places');
+const weather = require('./weather/weather');
 // console.log(argv.address); //after
 
-places.getPlacesLngLat(argv.address)
-  .then((response) => console.log(response));
+// places.getPlacesLngLat(argv.address)
+//   .then((response) => console.log(response));
+
+weather.getWeather(35.02, 139.01)
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err))
