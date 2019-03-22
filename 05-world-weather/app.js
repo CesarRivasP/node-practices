8092ad2eb70f8f10506cd80f528ec077aaf3f41e
CesarRivasP <cesarjose58@gmail.com>
2019-03-22 09:44:48 -0400

    Added require of places file and resolve promise of address

--- 05-world-weather/app.js
+++ 05-world-weather/app.js
@@ -1,3 +1,6 @@
 const argv = require('./config/yargs').argv;
-
+const places = require('./places/places')
 // console.log(argv.address); //after
+
+places.getPlacesLngLat(argv.address)
+  .then((response) => console.log(response));
