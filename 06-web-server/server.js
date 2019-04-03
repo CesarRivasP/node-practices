237b29e2f7ec6aa13fe47085acfb0981a6988f08
CesarRivasP <cesarjose58@gmail.com>
2019-04-03 12:37:25 -0400

    Implemented hbs with express

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -4,16 +4,28 @@ const app = express();  //Esto nos permite utilizar express
 // middleware -> use(especificar un callback -> express.static(folder que se quiere servir o que sea publico))
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (request, response) => {
-//   // response.send('hello world')
-//   let back = {
-//     name: 'Cesar',
-//     age: 23,
-//     url: request.url
-//   }
-//
-//   response.send(back)
-// });
+// Express hbs
+app.set('view engine', 'hbs');
+
+/*app.get('/', (request, response) => {  BEFORE, first example
+  // response.send('hello world')
+  let back = {
+    name: 'Cesar',
+    age: 23,
+    url: request.url
+  }
+
+  response.send(back)
+});*/
+
+//After - Second Example
+app.get('/', (request, response) => {
+
+  response.render('home', {
+    name: 'César',
+    anio: new Date().getFullYear()  //Para generar un año de forma dinamica
+  });
+});
 
 app.listen(3000, () => {
   console.log('escuchando peticiones en el puerto 3000');
