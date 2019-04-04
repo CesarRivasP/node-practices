797f6d7af3f0515ee0231414a89ff5582dd85481
CesarRivasP <cesarjose58@gmail.com>
2019-04-04 16:57:02 -0400

    Changes in the return of request

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express()
 
 app.get('/', (request, resolve) => {
-  resolve.send('Hello world');
+  // resolve.send('Hello world');
+  resolve.json('Hello world');
 })
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`Èscuchando por el puerto: 3000`);
+});
